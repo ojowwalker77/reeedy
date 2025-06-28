e7fac1480a8535868dba0ab7e932f8111e5b4481
Jonatas Walker <jonatasswwalker>
2025-06-28 12:52:00 -0300

    Initial Commit

---
+++ reeedyTests/reeedyTests.swift
@@ -0,0 +1,17 @@
+//
+//  reeedyTests.swift
+//  reeedyTests
+//
+//  Created by jonataswalker on 28/06/25.
+//
+
+import Testing
+@testable import reeedy
+
+struct reeedyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
